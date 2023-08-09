57495f2a57f87378b09675d6c0de86fb3c85165d
mstfygci <mustafa.1643@gmail.com>
2023-08-10 01:57:13 +0300

    fix(layout): changed key property to div

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,41 +1,47 @@
 import {Button, ButtonToolbar} from 'react-bootstrap';
 
 type ButtonType = {
+  key: string,
   title: string,
   backgroundColor: string,
   color: string
-}
+};
 
 export function Menu() {
   const buttons: ButtonType[] = [
     {
+      key: 'play',
       title: 'PLAY',
       backgroundColor: 'gainsboro',
       color: 'black'
     },
     {
+      key: 'inventory',
       title: 'INVENTORY',
       backgroundColor: 'gainsboro',
       color: 'black'
     },
     {
+      key: 'roles',
       title: 'ROLES',
       backgroundColor: 'gainsboro',
       color: 'black'
     }
-  ]
+  ];
 
   return (
     <ButtonToolbar
       className="position-absolute d-flex row start-50 top-50 translate-middle justify-content-center pe-0">
       {buttons.map((item, index) => {
         return (
-          <div className={`row justify-content-center ${index != 0 && ' mt-2'}`} key={`menu_${index}`}>
+          <div className={`row justify-content-center ${index != 0 && ' mt-2'}`}
+               key={`menu_${item.key}`}>
             <Button className="w-auto" variant="outline-dark"
                     style={{backgroundColor: item.backgroundColor}}>{item.title}</Button>
           </div>
-        )
+        );
       })}
     </ButtonToolbar>
   );
 }
+
